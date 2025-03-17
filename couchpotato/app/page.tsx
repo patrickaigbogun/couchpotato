3c310cd050cfe552bd906635fdc810d4997d3f86
Patrick Aigbogun <107092681+patrickaigbogun@users.noreply.github.com>
2025-03-17 18:01:27 +0100

    (feat): particles for the background

--- couchpotato/app/page.tsx
+++ couchpotato/app/page.tsx
@@ -1,12 +1,17 @@
-import { Suspense } from 'react';	
+import { Suspense } from 'react';
 import { MoviesGrid } from '@/components/reuse/moviesgrid';
 import { getLatestMovies } from '@/lib/tmdb/getLatestMovies';
+import { Box } from '@radix-ui/themes';
+import { BgParticles } from '@/components/ui/particle';
 
 export default async function Home() {
 	const movies = await getLatestMovies();
-		return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<MoviesGrid movies={movies.results} />
-		</Suspense>
+	return (
+		<Box>
+			<BgParticles />
+			<Suspense fallback={<div>Loading...</div>}>
+				<MoviesGrid movies={movies.results} />
+			</Suspense>
+		</Box>
 	);
 }
